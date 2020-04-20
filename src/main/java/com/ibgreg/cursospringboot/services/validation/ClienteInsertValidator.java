e24effc631555310e0f3753db3082e3054fdfeb4
Ítalo Gregório <italo-07@hotmail.com>
2020-04-20 19:47:58 -0300

    Adic. validação customizada de e-mail

--- src/main/java/com/ibgreg/cursospringboot/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/ibgreg/cursospringboot/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ibgreg.cursospringboot.domain.Cliente;
 import com.ibgreg.cursospringboot.domain.enums.TipoCliente;
 import com.ibgreg.cursospringboot.dto.ClienteNewDTO;
+import com.ibgreg.cursospringboot.repositories.ClienteRepository;
 import com.ibgreg.cursospringboot.resources.exception.FieldMessage;
 import com.ibgreg.cursospringboot.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +34,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCnpj(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "E-mail já cadastrado"));
 		}
 
 		for (FieldMessage e : list) {
