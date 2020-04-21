f8083e2ce74ea7599a91c12374b3c22c3f9b69a7
Ítalo Gregório <italo-07@hotmail.com>
2020-04-21 14:46:07 -0300

    Método POST para a inserção de Pedido

---
+++ src/main/java/com/ibgreg/cursospringboot/services/BoletoService.java
@@ -0,0 +1,19 @@
+package com.ibgreg.cursospringboot.services;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.ibgreg.cursospringboot.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+
+	public void preencherPagamentoComBoleto(PagamentoComBoleto pgto, Date instantePedido) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(instantePedido);
+		cal.add(Calendar.DAY_OF_MONTH, 7);
+		pgto.setDataVencimento(cal.getTime());
+	}
+}
