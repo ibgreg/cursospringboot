4b857ddad7e6740cd84a0fe78eec4d9fed1ca4bf
Ítalo Gregório <italo-07@hotmail.com>
2020-04-20 22:25:40 -0300

    Criado endpoint de Produtos com métodos GET

--- src/main/java/com/ibgreg/cursospringboot/repositories/ProdutoRepository.java
+++ src/main/java/com/ibgreg/cursospringboot/repositories/ProdutoRepository.java
@@ -1,11 +1,19 @@
 package com.ibgreg.cursospringboot.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ibgreg.cursospringboot.domain.Categoria;
 import com.ibgreg.cursospringboot.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	@Transactional(readOnly = true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
