a4abf8d22d61abb5fa54efe3b4f32efce5a15091
Ítalo Gregório <italo-07@hotmail.com>
2020-05-09 16:59:12 -0300

    Adic. serviço de envio de email via SMTP do Gmail

--- src/main/java/com/ibgreg/cursospringboot/config/DevConfig.java
+++ src/main/java/com/ibgreg/cursospringboot/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.ibgreg.cursospringboot.services.DBService;
+import com.ibgreg.cursospringboot.services.EmailService;
+import com.ibgreg.cursospringboot.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -30,5 +32,10 @@ public class DevConfig {
 		dbService.instantiateTestDatabase();
 		
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
