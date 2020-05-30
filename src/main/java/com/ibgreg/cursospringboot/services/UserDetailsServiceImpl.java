6c2c22f5506013399403120fbd032ef8aae84777
Ítalo Gregório <italo-07@hotmail.com>
2020-05-30 15:27:52 -0300

    Implantação de autenticação e geração do token JWT

---
+++ src/main/java/com/ibgreg/cursospringboot/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.ibgreg.cursospringboot.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.ibgreg.cursospringboot.domain.Cliente;
+import com.ibgreg.cursospringboot.repositories.ClienteRepository;
+import com.ibgreg.cursospringboot.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
