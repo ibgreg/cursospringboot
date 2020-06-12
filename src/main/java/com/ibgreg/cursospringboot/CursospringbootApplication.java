20576df60c383df02a9101703ac0a9b77b6570a0
Ítalo Gregório <italo-07@hotmail.com>
2020-06-12 18:07:26 -0300

    Criado endpoint para envio de imagens do perfil do cliente

--- src/main/java/com/ibgreg/cursospringboot/CursospringbootApplication.java
+++ src/main/java/com/ibgreg/cursospringboot/CursospringbootApplication.java
@@ -1,17 +1,11 @@
 package com.ibgreg.cursospringboot;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.ibgreg.cursospringboot.services.S3Service;
-
 @SpringBootApplication
 public class CursospringbootApplication implements CommandLineRunner{
-
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringbootApplication.class, args);
@@ -19,7 +13,6 @@ public class CursospringbootApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\testes3\\user2.jpg");
 	}
 
 }
