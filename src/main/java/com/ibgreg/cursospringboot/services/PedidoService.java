895953c32cbcf138faffb18169c838f18b0de29c
Ítalo Gregório <italo-07@hotmail.com>
2020-06-12 11:21:59 -0300

    Restrição da busca de pedidos para o usuário logado

--- src/main/java/com/ibgreg/cursospringboot/services/PedidoService.java
+++ src/main/java/com/ibgreg/cursospringboot/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ibgreg.cursospringboot.domain.Cliente;
 import com.ibgreg.cursospringboot.domain.ItemPedido;
 import com.ibgreg.cursospringboot.domain.PagamentoComBoleto;
 import com.ibgreg.cursospringboot.domain.Pedido;
@@ -14,6 +18,8 @@ import com.ibgreg.cursospringboot.domain.enums.EstadoPagamento;
 import com.ibgreg.cursospringboot.repositories.ItemPedidoRepository;
 import com.ibgreg.cursospringboot.repositories.PagamentoRepository;
 import com.ibgreg.cursospringboot.repositories.PedidoRepository;
+import com.ibgreg.cursospringboot.security.UserSS;
+import com.ibgreg.cursospringboot.services.exceptions.AuthorizationException;
 import com.ibgreg.cursospringboot.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -44,6 +50,18 @@ public class PedidoService {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		
+		return repo.findByCliente(cliente, pageRequest);
 	}
 	
 	@Transactional
