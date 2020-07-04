4c871c392ed58aa7e7b108f5f4f9b7355ddc089f
Ítalo Gregório <italo-07@hotmail.com>
2020-07-04 15:49:58 -0300

    Criado endpoint p/ busca de cliente por email

--- src/main/java/com/ibgreg/cursospringboot/services/ClienteService.java
+++ src/main/java/com/ibgreg/cursospringboot/services/ClienteService.java
@@ -76,6 +76,21 @@ public class ClienteService {
 		return repo.findAll(pageRequest);
 	}
 	
+	public Cliente findByEmail(String email) {
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+
+		Cliente obj = repo.findByEmail(email);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + user.getId() + ", Tipo: " + Cliente.class.getName());
+		}
+		
+		return obj;
+	}
+	
 	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
